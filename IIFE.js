63f194500d9c6c7e36c430014c8c23a9acf7cfdb
Hugo Rios <hugostaq@gmail.com>
2022-08-24 21:59:35 -0300

    IIFE - Imediatly Invoked Function Expression.

---
+++ IIFE.js
@@ -0,0 +1,7 @@
+// IIFE (Imediatly Invoked Function Expression) - Dessa forma uma determinada função pode ser executada imediatamente, assim que o código for iniciado.
+
+(function() {
+    console.log('Hello, World!')
+})();
+
+console.log('End.')
